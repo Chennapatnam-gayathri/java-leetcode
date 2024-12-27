c6bb4f0301f60a6604269cc26a3f6cbeb8e99d9f
Gayathri chennapatnam <152840303+Chennapatnam-gayathri@users.noreply.github.com>
2024-12-27 21:04:08 +0530

    Add files via upload

---
+++ obsolute.java
@@ -0,0 +1,13 @@
+public class obsolute {
+    
+        public static void main(String[] args) {
+            int temp=7-8;
+            System.out.println(temp);
+            if(temp<0){
+                temp=temp*-1;
+            }
+             System.out.println(temp);
+        }
+    }
+    
+
