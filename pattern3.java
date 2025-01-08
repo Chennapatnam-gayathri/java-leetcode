6af8ed364e29d1cf3f56130ae39c763319716a99
Gayathri chennapatnam <152840303+Chennapatnam-gayathri@users.noreply.github.com>
2025-01-08 06:53:16 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,25 @@
+public class pattern3 {
+    public static void main(String[] args){
+        int r=10;
+        int n=3;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<n;j++){
+                System.out.print("*");
+                if(j!=n-1){
+                    System.out.print("-");
+
+                }
+
+            }
+            System.out.println();
+        }
+                
+                
+            }
+            
+            
+        }
+       
+    
+    
+
